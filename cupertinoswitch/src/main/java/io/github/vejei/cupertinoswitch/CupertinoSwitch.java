694390c63f5efa0c690953f89282fd055fa5a532
vejei <chengsnem@163.com>
2020-11-04 17:01:06 +0800

    Create CupertinoSwitch.java

---
+++ cupertinoswitch/src/main/java/io/github/vejei/cupertinoswitch/CupertinoSwitch.java
@@ -0,0 +1,21 @@
+package io.github.vejei.cupertinoswitch;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CupertinoSwitch extends View {
+    public CupertinoSwitch(Context context) {
+        super(context);
+    }
+
+    public CupertinoSwitch(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CupertinoSwitch(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
