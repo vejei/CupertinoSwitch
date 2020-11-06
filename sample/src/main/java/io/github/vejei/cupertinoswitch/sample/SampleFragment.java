6a96aa1082ae61aa27ac629449d83d8d5f0d2902
vejei <chengsnem@163.com>
2020-11-06 21:46:36 +0800

    Create SampleFragment.java

---
+++ sample/src/main/java/io/github/vejei/cupertinoswitch/sample/SampleFragment.java
@@ -0,0 +1,26 @@
+package io.github.vejei.cupertinoswitch.sample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SampleFragment extends Fragment {
+    private static final String TAG = SampleFragment.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
