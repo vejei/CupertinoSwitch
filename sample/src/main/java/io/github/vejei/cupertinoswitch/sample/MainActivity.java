bbc3cffbed91f1221b767d24e42c544eddcd0ea7
vejei <chengsnem@163.com>
2020-11-11 11:55:47 +0800

    Add sample fragment

--- sample/src/main/java/io/github/vejei/cupertinoswitch/sample/MainActivity.java
+++ sample/src/main/java/io/github/vejei/cupertinoswitch/sample/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new SampleFragment())
+                    .commit();
+        }
     }
 }
